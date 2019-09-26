f6032afdf2e9d27ccaedb514ddad49fe1bdc6822
Gabriel Biasom <gabrielbiasom@hotmail.com>
2019-09-26 16:34:13 -0300

    Banco

--- Processual/src/ex2/ContaCorrenteSimplificada.java
+++ Processual/src/ex2/ContaCorrenteSimplificada.java
@@ -1,5 +1,42 @@
 package ex2;
 
+
 public class ContaCorrenteSimplificada {
+	String agencia;
+	String nroContaCorrente;
+	double saldo;
+	String nomeDoCorrentista;
+	boolean especial;
+	PessoaFisica[] pessoas = new PessoaFisica[2];
+	int qtdMax = 2;
+	int qtdAtual = 0;
+	
+	double sacar(double valor) {
+		if(valor > this.saldo && this.especial) {
+			System.out.println("Saldo Insuficiente para saque");
+		}
+		else {
+			if(this.saldo - valor > -5000)
+				this.saldo = this.saldo - valor;			
+		}
+		return this.saldo;
+	}
+	
+	double depositar(double valor) {
+		this.saldo = this.saldo + valor;
+		return saldo;
+	}
+	
+	void exibirSaldo() {
+		System.out.println("SALDO: "+ this.saldo);
+	}
+	
+	void adicionarPessoa(PessoaFisica p) {
+		if(qtdAtual<qtdMax) {
+			this.pessoas[qtdAtual] = p;
+			qtdAtual++;
+		}
+	}
+
 
 }
--- Processual/src/ex2/Principal.java
+++ Processual/src/ex2/Principal.java
@@ -2,4 +2,37 @@ package ex2;
 
 public class Principal {
 
+	public static void main(String[] args) {
+		ContaCorrenteSimplificada c1;
+		c1 = new ContaCorrenteSimplificada();
+		
+		
+		ContaCorrenteSimplificada c2 = new ContaCorrenteSimplificada();
+		
+		c1.agencia = "123";
+		c1.nroContaCorrente = "11115-2";
+		c1.nomeDoCorrentista =  "Thiago Magno";
+		c1.saldo = 50000.00;
+		c1.especial = false;
+		c1.sacar(52000);
+		
+		c2.agencia = "123";
+		c2.nroContaCorrente = "22227-3";
+		c2.nomeDoCorrentista = "Samuel Suassuna";
+		c2.saldo = 50000.00;
+		
+		 c1.sacar(500);
+		 c1.exibirSaldo();
+		 c2.exibirSaldo();
+		 
+		 c1 = c2;
+		 c1.exibirSaldo();
+		 c1.sacar(30000);
+		 c1.exibirSaldo();
+		 c2.exibirSaldo();
+		 
+		 double x = c2.sacar(200000);
+		 c2.exibirSaldo();
+
+
 }
